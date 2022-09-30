9ce030c8e431d931062b952b3c098b1f01436fe8
Oliver <oliver.bustamante@pucp.edu.pe>
2022-09-30 09:50:32 -0500

    intents

--- app/src/main/java/edu/pe/pucp/lab3_iot/MainActivity.java
+++ app/src/main/java/edu/pe/pucp/lab3_iot/MainActivity.java
@@ -2,6 +2,7 @@ package edu.pe.pucp.lab3_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,13 +11,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_main);
     }
 
     public void irRegistro(View view){
+        Intent intent = new Intent(this, RegistroActivity.class);
+        startActivity(intent);
     }
 
     public void irEmergencia(View view){
+        Intent intent = new Intent(this,EmergenciaActivity.class);
+        startActivity(intent);
     }
 
     public void irHistorial(View view){
